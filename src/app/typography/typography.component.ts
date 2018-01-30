415bb80dcaa0107fb5d743af30ddc59215b43127
JuniorOsho <web2ajax@gmail.com>
2018-01-30 03:16:07 -0200

    MP3 ADD

--- src/app/typography/typography.component.ts
+++ src/app/typography/typography.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {PessoasService} from 'app/services/pessoas.service';
+import {Pessoas} from 'app/model/pessoas';
+declare var $: any;
 @Component({
   selector: 'app-typography',
   templateUrl: './typography.component.html',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypographyComponent implements OnInit {
 
-  constructor() { }
+ 
+  public pessoas: Pessoas[] = [];
 
+  constructor(private pessoasService: PessoasService) { }
+  showNotification(from, align){
+    const type = ['','info','success','warning','danger'];
+
+    const color = Math.floor((Math.random() * 4) + 1);
+
+    $.notify({
+        icon: 'notifications',
+        message: 'ESCOLHA CERTA...</b> PLAY.'
+
+    },{
+        type: type[color],
+        timer: 4000,
+        placement: {
+            from: from,
+            align: align
+        }
+    });
+}
   ngOnInit() {
+    this.pessoasService.getPessoas()
+    .subscribe(data =>
+     this.pessoas = data);
   }
 
 }
